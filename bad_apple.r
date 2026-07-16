34813496b892f640e448022aaaae32ab1064baa9
elgikore <elgienbertc@gmail.com>
2026-07-16 18:58:45 +1200

    extract frames

--- bad_apple.r
+++ bad_apple.r
@@ -1,5 +1,18 @@
 install.packages(c("imager", "av"))
-library(imager, av)
+library(imager)
+library(av)
 
 FPS = 10
+source_file = "~/bad_apple.mp4"
+temp_frame_folder = "./frames"
 
+if (!dir.exists(temp_frame_folder)) {
+  paste("Created", temp_frame_folder)
+  dir.create(temp_frame_folder)
+}
+
+av_video_images(
+  source_file,
+  destdir = temp_frame_folder,
+  fps = FPS
+)
